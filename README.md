a6c7322543840639f6e96ec967e171dc2433a6e6
Rene Ismael <Ismael_br7@hotmail.com>
2020-04-16 04:04:21 -0500

    CRUD students, courses complete

---
+++ README.md
@@ -0,0 +1,15 @@
+# App de podcast
+
+Esta es una aplicacion creada con el framework express y graphql para poder crear estudiantes y cursos
+
+## Cómo funciona?
+
+Requiere nodeJs
+Instalar las dependencias
+
+- `npm install` para instalar las dependencias.
+- `npm run dev` para el entorno de desarrollo.
+
+## Licencia
+
+MIT
---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict'
+
+require('dotenv').config()
+
+const { makeExecutableSchema } = require('graphql-tools')
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+
+/* Resolvers */
+const resolvers = require('./lib/resolvers')
+
+const app = express()
+const port = process.env.port || 3000
+
+const typeDefs = readFileSync(
+  join(__dirname, 'lib', 'schema.graphql'),
+  'utf-8'
+)
+
+// consumiendo el schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers 
+})
+
+app.use(
+  '/api',
+  gqlMiddleware({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true
+  })
+)
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`)
+})
